32a206c7b7393feb223f597c79c70adb3717dbcb
Coyolx <142180783+Coyolx@users.noreply.github.com>
2025-10-27 12:36:28 +0300

    Update TwapTrap.sol

--- TwapTrap.sol
+++ TwapTrap.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;  
 
 interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);    
+    function balanceOf(address account) external view returns (uint256);   
 }
 
 contract TwapTrap {
